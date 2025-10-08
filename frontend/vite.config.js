909b7deb8532de403e85f513213292d1fe4d4b66
admarothiya <admarothiya@gmail.com>
2025-10-08 13:01:47 +0530

    vite me changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
  server: {
     proxy: {
       '/api': {
-        target: process.env.API_BASE_URL || 'http://localhost:5000',
+        target: "https://adcms.onrender.com"|| 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },

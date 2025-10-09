02a49e8dcf973ca5d9b42b0d08afd80466417f34
admarothiya <admarothiya@gmail.com>
2025-10-09 13:17:45 +0530

    hm

--- frontend/src/component/About.jsx
+++ frontend/src/component/About.jsx
@@ -38,7 +38,7 @@ function About() {
         </h1>
 
         {/* Cards Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 max-w-7xl mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-9 max-w-7xl mx-auto">
           {features.map((feature, index) => (
             <div
               key={index}
